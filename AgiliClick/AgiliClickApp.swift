45f37593e5cf2b3580eafa366d25539b9b232249
redhydratech <redhydratechnologies@gmail.com>
2021-06-11 17:07:52 -0400

    Initial Commit

---
+++ AgiliClick/AgiliClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgiliClickApp.swift
+//  AgiliClick
+//
+//  Created by Edward Kim on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AgiliClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
